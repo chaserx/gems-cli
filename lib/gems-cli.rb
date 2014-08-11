f75cef472b7824dee48fa0adc6e1155a5ee7adb9
Chase Southard <chase.southard@gmail.com>
2014-08-10 22:26:38 -0400

    more requirements

--- lib/gems-cli.rb
+++ lib/gems-cli.rb
@@ -1,4 +1,8 @@
 require 'gems-cli/version.rb'
+require 'gems'
+require 'will_paginate/collection'
+require 'will_paginate/array'
+require 'highline/import'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
