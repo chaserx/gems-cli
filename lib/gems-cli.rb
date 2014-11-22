511d5df849a56a2d9c15f66d2afebf5425ab0594
Chase Southard <chase.southard@gmail.com>
2014-11-22 16:09:33 -0500

    include pagination lib

--- lib/gems-cli.rb
+++ lib/gems-cli.rb
@@ -1,8 +1,9 @@
-require 'gems-cli/version.rb'
 require 'gems'
 require 'highline/import'
 require 'clipboard'
 require 'rainbow/ext/string'
-
+require 'will_paginate/array'
+require 'gems-cli/version.rb'
+require 'gems-cli/paginator.rb'
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
